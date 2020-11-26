2faf1b9667e5435f02451750237eae19b8b78b71
Sonja Tilly <sonjatilly@web.de>
2020-11-26 15:33:27 +0000

    Update metric.py

--- bipnet/metric.py
+++ bipnet/metric.py
@@ -47,5 +47,6 @@ if __name__=='__main__':
 	x = pd.read_csv(x,sep='\t',header=None)
 	A = awi(x[0],x[1])
 	
-	print("AWI: ",A[0])
-	print("pvalue: ",A[1])
+	#print("AWI: ",A[0])]
+	print('hello')
+	#print("pvalue: ",A[1])
